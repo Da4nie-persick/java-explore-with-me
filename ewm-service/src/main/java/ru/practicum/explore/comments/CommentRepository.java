be376ba2842e4a074463437082086625462aceca
Da4nie-persick <darya.tcheltygmasheva@yandex.ru>
2023-10-12 09:34:36 +0700

    диплом. часть 3

--- ewm-service/src/main/java/ru/practicum/explore/comments/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/explore/comments/CommentRepository.java
@@ -13,7 +13,10 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     @Query(" select c from Comment c " +
             "where upper(c.text) like upper(concat('%', ?1, '%')) ")
     List<Comment> searchComment(String text, Pageable pageable);
+
     List<Comment> findAllByEventIdOrderByCreated(Integer eventId, Pageable pageable);
+
     List<Comment> findAllByAuthorIdOrderByCreated(Integer authorId, Pageable pageable);
+
     List<Comment> findAllByAuthorIdAndEventIdOrderByCreated(Integer authorId, Integer eventId, Pageable pageable);
 }
--- ewm-service/src/main/java/ru/practicum/explore/comments/model/Comment.java
+++ ewm-service/src/main/java/ru/practicum/explore/comments/model/Comment.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import ru.practicum.explore.events.model.Event;
 import ru.practicum.explore.user.model.User;
 
